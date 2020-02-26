c959f3f0ec0295e4be1c9d438b249252bdfae6a7
brunomrlima <bruno.mrlima@gmail.com>
2020-02-26 11:27:07 -0500

    adding basic model tests for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,4 +3,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:first_user)
+  end
+
+  test 'valid user' do
+    assert @user.valid?
+  end
+
+  test 'invalid without name' do
+    @user.name = nil
+    refute @user.valid?, 'user is valid without a name'
+    assert_not_nil @user.errors[:name], 'no validation error for name present'
+  end
+
+  test 'invalid without email' do
+    @user.email = nil
+    refute @user.valid?
+    assert_not_nil @user.errors[:email]
+  end
+
 end
