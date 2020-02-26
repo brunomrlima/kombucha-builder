b045af98bbb9c181b320c25f4bfcbd0e9306ed79
brunomrlima <bruno.mrlima@gmail.com>
2020-02-26 13:10:26 -0500

    adding basic kombuchas testing and fixing update testing

--- test/controllers/api/v1/kombuchas_controller_test.rb
+++ test/controllers/api/v1/kombuchas_controller_test.rb
@@ -3,48 +3,47 @@
 require 'test_helper'
 
 class Api::V1::KombuchasControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @kombucha = kombuchas(:guinny_pop)
+  end
+
+  test "index should work" do
+    get '/api/v1/kombuchas', params: {}, headers: headers
+    assert_response :success
+  end
+
   test "returns collection of kombuchas" do
     get '/api/v1/kombuchas', params: {}, headers: headers
-
     assert_equal response_body.length, Kombucha.count
   end
 
   test "shows one kombucha" do
-    kombucha = kombuchas(:guinny_pop)
-
-    get "/api/v1/kombuchas/#{kombucha.id}", headers: headers
-
-    assert_equal response_body['id'], kombucha.id
+    get "/api/v1/kombuchas/#{@kombucha.id}", headers: headers
+    assert_equal response_body['id'], @kombucha.id
   end
 
   test "creates a new kombucha" do
     post "/api/v1/kombuchas", params: kombucha_params, headers: headers
-
     assert_response :success
     assert_not_nil Kombucha.find(response_body['id'])
   end
 
   test "does not create kombucha if fizziness level is invalid" do
     post "/api/v1/kombuchas", params: invalid_kombucha_params, headers: headers
-
     assert_response :unprocessable_entity
     assert_not_nil response_body['errors']
   end
 
   test "updates a kombucha" do
-    kombuchas(:guinny_pop)
-
-    post "/api/v1/kombuchas", params: kombucha_params, headers: headers
-
+    patch "/api/v1/kombuchas/#{@kombucha.id}", params: kombucha_params, headers: headers
     assert_response :success
-    assert_not_nil Kombucha.find(response_body['id'])
+    @kombucha.reload
+    assert_equal kombucha_params[:kombucha][:name], @kombucha.name
   end
 
   test "does not update kombucha if fizziness level is invalid" do
-    kombucha = kombuchas(:guinny_pop)
-
-    patch "/api/v1/kombuchas/#{kombucha.id}", params: invalid_kombucha_params, headers: headers
-
+    patch "/api/v1/kombuchas/#{@kombucha.id}", params: invalid_kombucha_params, headers: headers
     assert_response :unprocessable_entity
     assert_not_nil response_body['errors']
   end
