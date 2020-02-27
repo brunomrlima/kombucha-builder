83e303249f5874e4c95034b6fd8c746771e582c7
brunomrlima <bruno.mrlima@gmail.com>
2020-02-27 13:16:08 -0500

    show is now available

--- app/controllers/api/v1/flights_controller.rb
+++ app/controllers/api/v1/flights_controller.rb
@@ -1,19 +1,17 @@
-# frozen_string_literal: true
-
 class Api::V1::FlightsController < ApiController
   before_action :authenticate_user!
   skip_before_action :verify_authenticity_token, :except => [:update, :create]
-  protect_from_forgery with: :null_session, only: [:update, :create]
-  # before_action :set_rating, only:[:show, :update]
+  protect_from_forgery with: :null_session, only: [:create]
   #
   def index
     @flights = Flight.all
     render json: @flights.map(&:to_h), status: :ok
   end
-  #
-  # def show
-  #     render json: @rating.to_h
-  # end
+
+  def show
+    @flight = Flight.find_by_id(params[:id])
+    render json: @flight.to_h
+  end
 
   def create
     kombuchas_samples_ids = Kombucha.n_samples_with_different_base(4)
@@ -27,27 +25,5 @@ class Api::V1::FlightsController < ApiController
       render json: { errors: flight.errors }, status: :unprocessable_entity
     end
   end
-  #
-  # def update
-  #   if @rating.present?
-  #     if @rating.update(rating_params)
-  #       render json: @rating.to_h
-  #     else
-  #       render json: { errors: @rating.errors }, status: :unprocessable_entity
-  #     end
-  #   else
-  #     render json: { errors: "Couldn't find Rating" }, status: :unprocessable_entity
-  #   end
-  # end
-  #
-  # private
-  #
-  #   def set_rating
-  #     @rating = current_user.ratings.find_by_id(params[:id])
-  #   end
-  #
-  #   def rating_params
-  #     params.permit(:score, :kombucha_id)
-  #   end
 
 end
